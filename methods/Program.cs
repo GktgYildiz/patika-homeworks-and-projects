e884e31870bc10e17d65c0d144dd3b7388beea80
Goktug YILDIZ <102765970+GktgYildiz@users.noreply.github.com>
2022-04-26 13:56:40 +0300

    Add files via upload

---
+++ methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // erişim_belirtici geri_donustipi metot_adş(parametreListesi/arguman)
+            //{
+            ////komutlar;
+            // return;
+            //}
+
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a + b); //1. yol
+
+
+            int sonuc =Topla(a, b);
+            Console.WriteLine(sonuc);  //2. yol
+            
+            Metotlar ornek=new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc)); //3. yol
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b); //4. yol
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a + b));
+
+
+        }
+
+        // called by value // -> Değer tipinde atama
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir (string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        //called by reference// -> referans ile atama
+        public int ArttirVeTopla (ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1+deger2;
+        }
+    }
+}
